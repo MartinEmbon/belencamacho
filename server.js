2753a6b5ba45d37f80ed265db833b99ec5a170d2
Martin Embon <martinembon@hotmail.com>
2023-12-02 20:45:15 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const nodemailer = require("nodemailer")
+const path = require('path')
+const app=express()
+
+app.use(express.static('public'))
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+
+})
+
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:"martinembon9@gmail.com",
+            pass:"yuhh tvbo qqav hsqq"
+        }
+    })
+    const mailOptions = {
+        from:req.body.email,
+        to:"martinembon9@gmail.com",
+        subject:`Mensaje de ${req.body.email}`,
+        text:req.body.message
+    }
+    transporter.sendMail(mailOptions,(error,info)=>{
+        if(error){
+            console.log(error)
+            res.send('error')
+        }
+        else{
+            console.log("Email enviado " + info.response)
+            res.send('success')
+        }
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log("Server running")
+})
